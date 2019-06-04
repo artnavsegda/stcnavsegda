f6f1c1fe77acdee50a105ae831b21ccba557bcc9
Art Navsegda <artnavsegda@gmail.com>
2019-06-04 16:42:54 +0300

    filll

--- src/filter.c
+++ src/filter.c
@@ -2,7 +2,21 @@
 #include <stdint.h>
 #include "filter.h"
 
-uint16_t filter(uint16_t input)
+#define HOLDSIZE 256
+
+struct holder_s
+{
+  int index;
+  uint16_t cbuffer[HOLDSIZE];
+};
+
+typedef struct holder_s holder_t;
+
+void filter_init(holder_t holder, uint16_t )
+{
+}
+
+uint16_t filter(holder_t holder, uint16_t input)
 {
 }
 
